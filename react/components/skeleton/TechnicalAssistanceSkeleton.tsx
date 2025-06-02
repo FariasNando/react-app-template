9699535795cc96155a69302b20dc6b8b96f20034
Luís Fernando <lu.is.fernando@hotmail.com>
2025-06-01 23:37:44 -0300

    fix skeleton

--- react/components/skeleton/TechnicalAssistanceSkeleton.tsx
+++ react/components/skeleton/TechnicalAssistanceSkeleton.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 
 const TechnicalAssistanceSkeleton: React.FC = () => (
-  <div className="ba b--light-gray br2 pa3 mb3 bg-light-gray animate-pulse shadow-1">
-    <div className="h1 w5 br2 bg-moon-gray mb2" />
-    <div className="h1 w4 br2 bg-moon-gray mb2" />
-    <div className="h1 w6 br2 bg-moon-gray mb2" />
-    <div className="h1 w3 br2 bg-moon-gray mb2" />
-    <div className="h1 w4 br2 bg-moon-gray" />
-  </div>
+  <div
+    className="ba b--light-gray br2 pa4 mb4 bg-light-gray animate-pulse flex flex-column"
+    style={{ maxWidth: 600, minWidth: 320, height: 220 }}
+  />
 )
 
 export default TechnicalAssistanceSkeleton
